712f959d7440ef4a742aa90b9301ca7c232da1ba
Wanda-Elizabeth <wandaelizabeth@gmail.com>
2022-05-24 18:29:22 +0300

    First Commit

---
+++ app/src/main/java/ork/liesa/keeptrackLog/MainActivity.kt
@@ -0,0 +1,11 @@
+package ork.liesa.keeptrackLog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
