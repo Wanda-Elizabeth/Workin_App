cb77f846276efa6aa4c1324158325b8c5f0a5bb3
Wanda-Elizabeth <wandaelizabeth@gmail.com>
2022-05-31 19:30:03 +0300

    First Commit

---
+++ app/src/main/java/ork/liesa/keeptrackLog/LogInActivity.kt
@@ -0,0 +1,19 @@
+package ork.liesa.keeptrackLog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LogInActivity : AppCompatActivity() {
+//    lateinit var btnLogin:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+//        val intent= Intent(this,Sign_Up::class.java)
+//        startActivity(intent)
+//        finish()
+
+    }
+
+    }
--- app/src/main/java/ork/liesa/keeptrackLog/SplashActivity.kt
+++ app/src/main/java/ork/liesa/keeptrackLog/SplashActivity.kt
@@ -1,11 +1,15 @@
 package ork.liesa.keeptrackLog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val intent= Intent(this,LogInActivity::class.java)
+        startActivity(intent)
+        finish()
+
     }
 }
